fd002b28daeae2623a7d2bf57a9e61209e781893
ffzanini <devffzanini@gmail.com>
2024-01-25 01:57:09 -0300

    fix(scrollbar): adjusted scrollbar

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,13 +1,13 @@
 'use client'
 import { motion, AnimatePresence } from 'framer-motion'
 
-export function Wrapper({ children }: { children: React.ReactNode }) {
+export function Wrapper({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <AnimatePresence
       onExitComplete={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
     >
       <motion.section
-        className="container min-h-[calc(100vh-13.46rem)] items-center justify-center pt-24 md:pt-36"
+        className="container min-h-[calc(100vh-14.28rem)] items-center justify-center pt-24 md:pt-36 overflow-hidden"
         initial={{ x: 300, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         exit={{ x: 300, opacity: 0 }}
--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -3,7 +3,9 @@ import { ThemeProvider } from 'next-themes'
 import { Toaster } from 'react-hot-toast'
 import { InternacionalizaionProvider } from '@/context/internacionalization-context'
 
-export function AppProvider({ children }: { children: React.ReactNode }) {
+export function AppProvider({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
       <InternacionalizaionProvider>
