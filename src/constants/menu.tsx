f5e76e557d43e4954685bd82d3148b86f9c94506
ffzanini <devffzanini@gmail.com>
2024-09-04 19:32:41 -0300

    feat(navbar): added new structure idea to new navbar in menu items

--- src/constants/menu.tsx
+++ src/constants/menu.tsx
@@ -1,12 +1,27 @@
 type MenuItem = {
   name: string
   href: string
+  subItems?: MenuItem[]
 }
 
 export const menu: MenuItem[] = [
   {
     name: 'about',
     href: '/about',
+    subItems: [
+      {
+        name: 'presentation',
+        href: '/about/presentation',
+      },
+      {
+        name: 'testimonials',
+        href: '/about/testimonials',
+      },
+      {
+        name: 'stack',
+        href: '/about/stack',
+      },
+    ],
   },
   {
     name: 'projects',
