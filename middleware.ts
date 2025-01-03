40f234bd1a520bd4e213f7b7a1cf251fdafd3c61
ffzanini <devffzanini@gmail.com>
2025-01-03 12:41:42 -0300

    fix(middleware): adjusted redirect

--- middleware.ts
+++ middleware.ts
@@ -4,13 +4,19 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   const { pathname, hostname } = request.nextUrl
 
-  if (hostname === 'resume.ffzanini.dev' && pathname === '/') {
-    return NextResponse.redirect(new URL('/resume', request.nextUrl))
+  const isResumeSubdomain = hostname === 'resume.ffzanini.dev'
+
+  if (isResumeSubdomain && pathname === '/') {
+    return NextResponse.rewrite(new URL('/resume', request.url))
+  }
+
+  if (hostname === 'ffzanini.dev' && pathname === '/resume') {
+    return NextResponse.redirect('https://resume.ffzanini.dev')
   }
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/:path*'],
+  matcher: ['/resume', '/'],
 }
