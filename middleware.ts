ba14322406c80b9e546e50809bca58c0d4bff74e
ffzanini <devffzanini@gmail.com>
2025-01-03 12:52:27 -0300

    fix(middleware): adjusted redirect

--- middleware.ts
+++ middleware.ts
@@ -4,19 +4,17 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   const { pathname, hostname } = request.nextUrl
 
-  const isResumeSubdomain = hostname === 'resume.ffzanini.dev'
-
-  if (isResumeSubdomain && pathname === '/') {
+  // Verifica se está no subdomínio "resume.ffzanini.dev"
+  if (hostname === 'resume.ffzanini.dev' && pathname === '/') {
+    // Reescreve a requisição para carregar a rota "/resume"
     return NextResponse.rewrite(new URL('/resume', request.url))
   }
 
-  if (hostname === 'ffzanini.dev' && pathname === '/resume') {
-    return NextResponse.redirect('https://resume.ffzanini.dev')
-  }
-
+  // Continua normalmente para outras rotas
   return NextResponse.next()
 }
 
+// Configuração opcional para definir em quais rotas o middleware será aplicado
 export const config = {
-  matcher: ['/resume', '/'],
+  matcher: ['/'], // Aplica o middleware apenas na raiz
 }
